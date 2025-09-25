8e7ad3819e4f573d9e8d565a3fedfa455ab0488c
varun <varunchowdary23@gmail.com>
2025-09-25 18:22:51 +0530

    login & register components ui

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -1,26 +1,31 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule],
+  imports: [FormsModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
   loginForm!: FormGroup;
 
-  constructor(){
+  constructor() {
     this.loginForm = new FormGroup({
       username: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required)
-    })
+      password: new FormControl('', Validators.required),
+    });
   }
 
-  login(){
-    console.log(this.loginForm.value)
+  login() {
+    console.log(this.loginForm.value);
   }
-
 }
--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, RouterModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
+  registerForm!: FormGroup;
 
+  constructor() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  register() {
+    console.log(this.registerForm.value);
+  }
 }
