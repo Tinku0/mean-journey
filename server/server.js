f6da2abbd3132396e1ccfbea995021afc59d55dd
varun <varunchowdary23@gmail.com>
2025-09-25 18:59:37 +0530

    server changes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app  = express();
+const userRoutes = require('./routes/user');
+const { connectToDB } = require('./config/db');
+require('dotenv').config();
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log('App is running on PORT - ', PORT);
+    connectToDB();
 })
 
 app.use(express.json());
@@ -12,3 +16,5 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("Back end API is running...");
 });
+
+app.use('/user', userRoutes);
